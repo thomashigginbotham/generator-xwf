5c895ec4594b8f5eef085660b694336252427f29
Thomas Higginbotham <thomas@thomashigginbotham.com>
2014-08-09 23:44:08 -0400

    Add support for Jasmine and Karma

--- app/index.js
+++ app/index.js
@@ -7,76 +7,90 @@ var chalk = require('chalk');
 
 
 var XwfGenerator = yeoman.generators.Base.extend({
-  init: function () {
-    this.pkg = require('../package.json');
+	init: function () {
+		this.pkg = require('../package.json');
 
-    this.on('end', function () {
-      if (!this.options['skip-install']) {
-        this.installDependencies();
-      }
-    });
-  },
+		this.on('end', function () {
+			if (!this.options['skip-install']) {
+				this.installDependencies();
+			}
+		});
+	},
 
-  askFor: function () {
-    var done = this.async();
+	askFor: function () {
+		var done = this.async();
 
-    // Have Yeoman greet the user.
-    this.log(yosay('Welcome to the marvelous XWF generator!'));
+		// Have Yeoman greet the user.
+		this.log(yosay('Welcome to the marvelous XWF generator!'));
 
-    var prompts = [{
-      name: 'appName',
-      message: 'What is the name of your app?'
-    }];
+		var prompts = [{
+			name: 'appName',
+			message: 'What is the name of your web site/app?'
+		}, {
+			type: 'confirm',
+			name: 'addJasmineKarma',
+			message: 'Would you like to add support for Jasmine and Karma?',
+			default: true
+		}];
 
-    this.prompt(prompts, function (props) {
-      this.appName = props.appName;
+		this.prompt(prompts, function (props) {
+			this.appName = props.appName;
+			this.addJasmineKarma = props.addJasmineKarma;
 
-      done();
-    }.bind(this));
-  },
+			done();
+		}.bind(this));
+	},
 
-  app: function () {
-    this.mkdir('app');
-    this.mkdir('app/javascripts');
-    this.mkdir('app/javascripts/lib');
-    this.mkdir('app/javascripts/lib/jquery-plugins');
-    this.mkdir('app/stylesheets');
-    this.mkdir('app/stylesheets/modules');
-    this.mkdir('app/stylesheets/partials');
-    this.mkdir('app/stylesheets/vendor');
+	app: function () {
+		this.mkdir('app');
+		this.mkdir('app/javascripts');
+		this.mkdir('app/javascripts/lib');
+		this.mkdir('app/javascripts/lib/jquery-plugins');
+		this.mkdir('app/stylesheets');
+		this.mkdir('app/stylesheets/modules');
+		this.mkdir('app/stylesheets/partials');
+		this.mkdir('app/stylesheets/vendor');
 
-    this.template('_index.html', 'app/index.html');
+		this.template('_index.html', 'app/index.html');
 
-    this.copy('_app.js', 'app/javascripts/app.js');
-    this.copy('_main.js', 'app/javascripts/main.js');
-    this.copy('_jquery.sampleplugin.js', 'app/javascripts/lib/jquery-plugins/jquery.sampleplugin.js');
-    this.copy('_main.scss', 'app/stylesheets/main.scss');
-    this.copy('_all.scss', 'app/stylesheets/modules/_all.scss');
-    this.copy('_framework.scss', 'app/stylesheets/modules/_framework.scss');
-    this.copy('_sprites-2x.scss', 'app/stylesheets/modules/_sprites-2x.scss');
-    this.copy('_base.scss', 'app/stylesheets/partials/_base.scss');
-    this.copy('_content.scss', 'app/stylesheets/partials/_content.scss');
-    this.copy('_footer.scss', 'app/stylesheets/partials/_footer.scss');
-    this.copy('_global.scss', 'app/stylesheets/partials/_global.scss');
-    this.copy('_grids.scss', 'app/stylesheets/partials/_grids.scss');
-    this.copy('_header.scss', 'app/stylesheets/partials/_header.scss');
-    this.copy('_resets.scss', 'app/stylesheets/partials/_resets.scss');
-    this.copy('_typography.scss', 'app/stylesheets/partials/_typography.scss');
+		this.copy('_app.js', 'app/javascripts/app.js');
+		this.copy('_main.js', 'app/javascripts/main.js');
+		this.copy('_jquery.sampleplugin.js', 'app/javascripts/lib/jquery-plugins/jquery.sampleplugin.js');
+		this.copy('_main.scss', 'app/stylesheets/main.scss');
+		this.copy('_all.scss', 'app/stylesheets/modules/_all.scss');
+		this.copy('_framework.scss', 'app/stylesheets/modules/_framework.scss');
+		this.copy('_sprites-2x.scss', 'app/stylesheets/modules/_sprites-2x.scss');
+		this.copy('_base.scss', 'app/stylesheets/partials/_base.scss');
+		this.copy('_content.scss', 'app/stylesheets/partials/_content.scss');
+		this.copy('_footer.scss', 'app/stylesheets/partials/_footer.scss');
+		this.copy('_global.scss', 'app/stylesheets/partials/_global.scss');
+		this.copy('_grids.scss', 'app/stylesheets/partials/_grids.scss');
+		this.copy('_header.scss', 'app/stylesheets/partials/_header.scss');
+		this.copy('_resets.scss', 'app/stylesheets/partials/_resets.scss');
+		this.copy('_typography.scss', 'app/stylesheets/partials/_typography.scss');
 
-    this.copy('_bower.json', 'bower.json');
-    this.copy('_config.rb', 'config.rb');
-    this.copy('_gruntfile.js', 'Gruntfile.js');
-    this.copy('_package.json', 'package.json');
-    this.copy('_readme.md', 'README.md');
+		this.copy('_bower.json', 'bower.json');
+		this.copy('_config.rb', 'config.rb');
+		this.copy('_gruntfile.js', 'Gruntfile.js');
+		this.copy('_package.json', 'package.json');
+		this.copy('_readme.md', 'README.md');
 
-    this.copy('bowerrc', '.bowerrc');
-    this.copy('gitignore', '.gitignore');
-  },
+		this.copy('bowerrc', '.bowerrc');
+		this.copy('gitignore', '.gitignore');
 
-  projectfiles: function () {
-    this.copy('editorconfig', '.editorconfig');
-    this.copy('jshintrc', '.jshintrc');
-  }
+		if (this.addJasmineKarma) {
+			this.mkdir('test');
+
+			this.copy('test/_sampleSpec.js', 'test/sampleSpec.js');
+			this.copy('test/_test-main.js', 'test/test-main.js');
+			this.copy('karma.conf.js', 'karma.conf.js');
+		}
+	},
+
+	projectfiles: function () {
+		this.copy('editorconfig', '.editorconfig');
+		this.copy('jshintrc', '.jshintrc');
+	}
 });
 
 module.exports = XwfGenerator;
